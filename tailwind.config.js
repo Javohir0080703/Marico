b94931ea2c39384cd2b6e27daef5fa2e022e8e1d
Javohir0080703 <renobobopocta@gmail.com>
2024-03-09 15:42:11 +0500

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,13 @@ module.exports = {
   theme: {
    extend:{
       colors:{
-     'white-black':'rgba(29, 31, 35, 0.70)',
-     '#1D1F23':'#1D1F23',
-     'qulrang':'rgba(57, 59, 63, 0.31)',
-     '#8D8D8D':'#8D8D8D',
-     '#F6973F':'#F6973F',
-     '#D6DEE2':'#D6DEE2',
-     '#FAFAFA':'#FAFAFA',
-     '#EF43580':'#EF4358',
-     '#19191B':'#19191B',
-     'qulrang2':'rgba(255, 255, 255, 0.70)'
+          "#707070":"#707070",
+          "#306EF7":"#306EF7",
+          "#3C82F4":"#3C82F4"
       },
       boxShadow:{
         'korzinkashadow':'-30px 15px 50px 0px rgba(0, 0, 0, 0.15)'
       }
    },
-    textDecorationThickness:{
-      textDecoration:{
-        'strikethrough':'strikethrough',
-      }
-    },
-    fontFamily:{
-      'liana':'Liana'
-    }
   },
 }
