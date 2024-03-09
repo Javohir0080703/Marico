1419805dd881ecf351fd48a2e24a8bcab5c6017d
Javohir0080703 <renobobopocta@gmail.com>
2024-03-10 00:35:32 +0500

    add faq section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
       colors:{
           "#707070":"#707070",
           "#306EF7":"#306EF7",
-          "#3C82F4":"#3C82F4"
+          "#3C82F4":"#3C82F4",
+          "#FE6783":"#FE6783",
+          "#FFBF84":"#FFBF84",
+          "#326CF9":"#326CF9"
       },
       boxShadow:{
         'korzinkashadow':'-30px 15px 50px 0px rgba(0, 0, 0, 0.15)'
