d4f32d504a4ee21b8d20f4ffd2642932f123998e
Javohir0080703 <renobobopocta@gmail.com>
2024-03-10 19:29:13 +0500

    add steps

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
           "#3C82F4":"#3C82F4",
           "#FE6783":"#FE6783",
           "#FFBF84":"#FFBF84",
-          "#326CF9":"#326CF9"
+          "#326CF9":"#326CF9",
+          "#3D82F2":"#3D82F2",
+          "#2D2D2D":"#2D2D2D",
+          "#0FC65C":"#0FC65C",
+          "#0D0D0D":"#0D0D0D",
+          "#919191":"#919191"
       },
       boxShadow:{
         'korzinkashadow':'-30px 15px 50px 0px rgba(0, 0, 0, 0.15)'
