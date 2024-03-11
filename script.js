574ef674c441ee6dace00ac5584c105a730fb982
Javohir0080703 <renobobopocta@gmail.com>
2024-03-11 11:12:26 +0500

    add hero responsive

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const box = document.querySelector(".js-header-box")
+const ElOpenBox = document.querySelector(".js-ElOpenBox")
+const ElCloseBox = document.querySelector(".js-ElCloseBox")
+
+
+
+ElOpenBox.addEventListener("click", function () {
+    box.style.display = 'block'
+    box.style.display = 'transition: all 0.3s ease'
+})
+
+ElCloseBox.addEventListener("click", function () {
+    box.style.display = 'none'
+})
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,
     centeredSlides: true,
-    loop: true,
     slidesPerView: 4,
+    loop: true,
     autoplay: {
         delay: 2000,
         disableOnInteraction: false,
     },
-    
-    
-});
+}); 
